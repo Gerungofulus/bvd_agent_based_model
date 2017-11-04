07b46413f9246f18462eed8efc3fa3642e1f3f50
Pascal Blunk <pascal.blunk@calimoto.eu>
2017-11-04 23:36:59 +0100

    Made changes to the Makefile in order to allow for testing with catch and fakeit

--- config.h
+++ config.h
@@ -5,16 +5,16 @@
 #define HAVE_INTTYPES_H 1
 
 /* Define to 1 if you have the `gsl' library (-lgsl). */
-/* #undef HAVE_LIBGSL */
+#define HAVE_LIBGSL 1
 
 /* Define to 1 if you have the `gslcblas' library (-lgslcblas). */
 #define HAVE_LIBGSLCBLAS 1
 
 /* Define to 1 if you have the `hdf5' library (-lhdf5). */
-/* #undef HAVE_LIBHDF5 */
+#define HAVE_LIBHDF5 1
 
 /* Define to 1 if you have the `hdf5_hl' library (-lhdf5_hl). */
-/* #undef HAVE_LIBHDF5_HL */
+#define HAVE_LIBHDF5_HL 1
 
 /* Define to 1 if you have the `m' library (-lm). */
 #define HAVE_LIBM 1
