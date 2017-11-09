de75dd7414c27be7020187ad023963da1a35814f
Pascal Blunk <pascal.blunk@calimoto.eu>
2017-11-09 19:03:53 +0100

    Expanded the utillities:map for events in order to have a proper output printed for debugging

--- src/Utilities.cpp
+++ src/Utilities.cpp
@@ -29,21 +29,29 @@ const std::map< Event_Type  , std::string > Utilities::Event_tostr =
    {Event_Type::INFECTION   ,"INFECTION"},
    {Event_Type::RECOVERY    ,"RECOVERY"},
    {Event_Type::TRADE       ,"TRADE"},
+   {Event_Type::REMOVECOW	  ,"REMOVECOW"}	,
    {Event_Type::SLAUGHTER	,"SLAUGHTER"},
    {Event_Type::CULLING		,"CULLING"},
    {Event_Type::VACCINATE	,"VACCINATE"},
    {Event_Type::END_OF_VACCINATION	,"END_OF_VACCINATION"},
    {Event_Type::LOG_OUTPUT  ,"LOG_OUTPUT"},
    {Event_Type::WRITE_OUTPUT,"WRITE_OUTPUT"},
+   {Event_Type::STOP        ,"STOP"},
    {Event_Type::MANAGE		,"MANAGE"},
    {Event_Type::EARTAG		,"EARTAG"},
    {Event_Type::TEST		,"TEST"},
    {Event_Type::ANTIBODYTEST,"ANTIBODYTEST"},
    {Event_Type::VIRUSTEST	,"VIRUSTEST"},
    {Event_Type::VACCINATION	,"VACCINATION"},
-   {Event_Type::STOP        ,"STOP"}};
+   {Event_Type::QUARANTINEEND, "QUARANTINEEND"},
+   {Event_Type::JUNGTIER      , "JUNGTIER"},
+   {Event_Type::JUNGTIER_EXEC	,"JUNGTIER_EXEC"},
+   {Event_Type::JUNGTIER_SMALL_GROUP, "JUNGTIER_SMALL_GROUP"},
+   {Event_Type::ChangeContainmentStrategy, "ChangeContainmentStrategy"}
+   };
 
-  
+
+
 void Utilities::pretty_print( Cow* c , std::ostream& out )
 {
   out << "id: " << c->id() << ", Birth time: " << c->birth_time  << " female: " << c->female << std::endl;
@@ -58,7 +66,7 @@ void Utilities::pretty_print( const Event* e , std::ostream& out)
 }
 
 void Utilities::printStackTrace(int number){
-	
+
 	char **strings;
 	void *array[number];
 	size_t size;
@@ -77,5 +85,5 @@ void Utilities::printStackTrace(int number){
 	printf("%s\n", strings[j]);
   	//backtrace_symbols_fd(array, size, STDERR_FILENO);
   	//exit(1);
-	
+
 }
